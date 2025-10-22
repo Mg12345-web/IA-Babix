ca22750218c75f8057e324d1050566b3aa833c4a
Mg12345-web <contatomgmultas@gmail.com>
2025-10-22 16:11:46 -0300

    Create __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,9 @@
+--- backend/main.py
++++ backend/main.py
+@@
+-from raciocinio import evaluate_topic
+-from memoria import Memoria
+-from utils import ensure_db, log_action
++from .raciocinio import evaluate_topic
++from .memoria import Memoria
++from .utils import ensure_db, log_action
