e63443a5ff03682a6ddf691618eb2e6ea670d762
Mg12345-web <contatomgmultas@gmail.com>
2025-10-24 12:03:08 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-apt-get update && apt-get install -y poppler-utils tesseract-ocr
 pip install -r requirements.txt
-python -m uvicorn backend.main:app --host 0.0.0.0 --port 8080
+uvicorn backend.main:app --host 0.0.0.0 --port ${PORT:-8080}
